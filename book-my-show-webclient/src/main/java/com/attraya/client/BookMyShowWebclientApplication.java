865c47e1aadf8590e6fbaf57ae5a585005d5c191
Attraya Das <attrayaghoshdas@gmail.com>
2023-12-09 11:33:31 +0530

    Added SpringBoot WebClient application

---
+++ book-my-show-webclient/src/main/java/com/attraya/client/BookMyShowWebclientApplication.java
@@ -0,0 +1,13 @@
+package com.attraya.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowWebclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyShowWebclientApplication.class, args);
+	}
+
+}
---
+++ book-my-show-webclient/src/test/java/com/attraya/client/BookMyShowWebclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attraya.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowWebclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
